b881f5703bf9e89a013168ee19fca4ab0c3d045a
Teclit <teklitte@gmail.com>
2025-03-03 22:51:55 +0100

    Setup Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'teclit';
+
+
+  activeLink: string = 'Accueil';
+
+  setActiveLink(link: string): void {
+    this.activeLink = link;
+  }
+
+
 }
