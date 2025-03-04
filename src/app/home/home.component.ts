88a0957833ef831952dba5c2f1378ff106130084
Teclit <teklitte@gmail.com>
2025-03-04 22:14:24 +0100

    update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,38 @@
-import {Component} from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    NgOptimizedImage
-  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  private readonly CV_FILE_URL = 'assets/docs/CV_Teklit_TEWOLDE_Developpeur_Java.pdf';
+  private readonly CV_FILE_NAME = 'CV_Teklit_TEWOLDE_Developpeur_Java.pdf';
+  linkedLink: string = 'https://www.linkedin.com/in/teklittewolde';
 
+  onDownloadCV(): void {
+    this.checkFileExists(this.CV_FILE_URL)
+      .then(() => this.downloadFile(this.CV_FILE_URL, this.CV_FILE_NAME))
+      .catch(error => console.error('Download failed:', error));
+  }
+
+  private downloadFile(fileUrl: string, fileName: string): void {
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.setAttribute('download', fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  private async checkFileExists(fileUrl: string): Promise<void> {
+    try {
+      const response = await fetch(fileUrl, { method: 'HEAD' });
+      if (!response.ok) {
+        throw new Error(`File not found: ${fileUrl}`);
+      }
+    } catch (error) {
+      throw new Error(`Error checking file: ${error instanceof Error ? error.message : String(error)}`);
+    }
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,18 +8,27 @@ import {Component, Input} from '@angular/core';
 export class NavbarComponent {
   @Input() title!: string;
   activeLink: string = 'Accueil';
+  menuOpen = false;
 
   setActiveLink(link: string, event: Event): void {
     event.preventDefault();
     this.activeLink = link;
-    this.menuOpen = false;
+
+    // Close the menu after clicking a link (for mobile view)
+    if (window.innerWidth <= 730) {
+      this.menuOpen = false;
+    }
   }
 
-  menuOpen = false;
-
-  toggleMenu() {
+  toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
   }
 
-
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    // Automatically close the menu on resizing to a larger screen
+    if (window.innerWidth > 730) {
+      this.menuOpen = false;
+    }
+  }
 }
